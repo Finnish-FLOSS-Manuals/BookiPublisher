7e84ff54be01cf7e51391eaf3397b3da289ace70
Tomi Toivio <tomi@flossmanuals.net>
2012-04-14 15:14:43 +0300

    Update _templates/fm_resources/fm_book.js

--- _templates/fm_resources/fm_book.js
+++ _templates/fm_resources/fm_book.js
@@ -3,17 +3,12 @@ function set_up_chapter_links() {
     var chapters = [];
     var titles = [];
     var here = document.location;
-
-var pathname = document.location.pathname.substring(1);
-var parts = pathname.split(/\//);
-if (parts.length > 1 ) {
-    var result = parts[1];
-    if (result =='') here += 'index/';
-}
+    if (/\/$/.test(here)){
+        here += 'index';
+    }
 
     $(".menu-goes-here a").each(function (i) {
- var thispage = this.href + '/';
-                                    if (thispage == here){
+                                    if (this.href == here){
                                         $(this).parent().css('background', '#f99b1c');
                                     }
                                     chapters.push(this.href);
@@ -24,7 +19,7 @@ if (parts.length > 1 ) {
     };
 
     for (var i = 0; i < chapters.length; i++){
-        if (chapters[i] + '/' == here){
+        if (chapters[i] == here){
             if (i > 0){
                 $('.left.arrow a').each(function() {
                                             this.href = chapters[i - 1];
@@ -50,4 +45,3 @@ if (parts.length > 1 ) {
 }
 
 var all_book_chapters = $(set_up_chapter_links);
-
